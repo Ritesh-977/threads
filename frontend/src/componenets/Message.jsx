15bcaffd4cf91e2655c9a50d3a69ca248705d78e
Ritesh Singh <yesfirst000@gmail.com>
2024-06-25 22:36:51 +0545

    image support added

--- frontend/src/componenets/Message.jsx
+++ frontend/src/componenets/Message.jsx
@@ -1,36 +1,61 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react'
-import { useRecoilValue } from 'recoil'
+import { Avatar, Box, Flex, Image, Text } from '@chakra-ui/react'
 import { selectedConversationAtom } from '../atoms/messagesAtom'
+import { useRecoilValue } from 'recoil'
 import userAtom from '../atoms/userAtom';
 import { BsCheck2All } from 'react-icons/bs';
 
 
-const Message = ({ownMessage, message}) => {
+const Message = ({ ownMessage, message }) => {
   const selectedConversation = useRecoilValue(selectedConversationAtom);
   const user = useRecoilValue(userAtom);
   return (
     <>
-    {ownMessage ?(
-    <Flex
-    gap={2}
-    alignSelf={'flex-end'}
-    >
-        <Flex bg={'green.800'} maxW={'350px'} p={1} borderRadius={'md'}>
-          <Text color={"white"}>{message.text}</Text>
-          <Box alignSelf={"flex-end"} ml={1} color={message.seen ? "blue.400" : ""} fontWeight={'bold'}>
-            <BsCheck2All size={16} />
-          </Box>
+      {ownMessage ? (
+        <Flex
+          gap={2}
+          alignSelf={'flex-end'} >
+
+          {message.text && (
+            <Flex bg={'green.800'} maxW={'350px'} p={1} borderRadius={'md'}>
+              <Text color={"white"}>{message.text}</Text>
+              <Box alignSelf={"flex-end"} ml={1} color={message.seen ? "blue.400" : ""} fontWeight={'bold'}>
+                <BsCheck2All size={16} />
+              </Box>
+            </Flex>
+          )}
+
+          {message.img && (
+            <Flex mt={5} w={'200px'}>
+              <Image
+                src={message.img}
+                alt='Message image'
+                borderRadius={4}
+              />
+            </Flex>
+          )}
+          
+          <Avatar src={user.profilePic} w={7} h={7} />
+
         </Flex>
-        <Avatar src={user.profilePic} w={7} h={7} />
-      
-    </Flex>
-    ): ( 
-        <Flex gap={2} >  
-            <Avatar src={selectedConversation.userProfilePic} w={7} h={7} />
+      ) : (
+        <Flex gap={2} >
+          <Avatar src={selectedConversation.userProfilePic} w={7} h={7} />
+          {message.text && (
             <Text maxW={'350px'} bg={'#2b2c2c'} p={1} borderRadius={'md'}>
-                 {message.text}
+              {message.text}
             </Text>
-        </Flex> )}
+          )}
+          {message.img && (
+            <Flex mt={5} w={'200px'}>
+              <Image
+                src={message.img}
+                alt='Message image'
+                borderRadius={4}
+              />
+            </Flex>
+          )}
+
+        </Flex>)}
     </>
   )
 }
--- frontend/src/componenets/MessageInput.jsx
+++ frontend/src/componenets/MessageInput.jsx
@@ -1,11 +1,13 @@
-import { Flex, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { Flex, FormControl, Image, Input, InputGroup, InputRightElement, Modal, ModalBody, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, Spinner, useDisclosure } from "@chakra-ui/react"
 import { IoSendSharp } from 'react-icons/io5';
 import Picker from "emoji-picker-react";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { MdEmojiEmotions } from "react-icons/md";
 import useShowToast from "../hooks/useShowToast";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 import { conversationsAtom, selectedConversationAtom } from "../atoms/messagesAtom";
+import { BsFillImageFill } from "react-icons/bs";
+import usePreviewImg from '../hooks/usePreviewImg';
 
 
 const MessageInput = ({setMessages}) => {
@@ -14,13 +16,22 @@ const MessageInput = ({setMessages}) => {
    const selectedConversation = useRecoilValue(selectedConversationAtom);
     const setConversations = useSetRecoilState(conversationsAtom)
     const [showPicker, setShowPicker] = useState(false);
+    const imageRef = useRef(null);
+    const {onClose} = useDisclosure();
+    const { handleImageChange, imgUrl, setImgUrl } = usePreviewImg();
+    const [isSending, setIsSending] = useState(false);
+
+
+
     const onEmojiClick = (event)=>{
         setMessageText((prevInput) => prevInput + event.emoji);
         setShowPicker(false);
     }
     const handleSendMessage = async (e) =>{
       e.preventDefault();
-      if(!messageText) return;
+      if(!messageText && !imgUrl) return;
+      if(isSending) return;
+      setIsSending(true);
       try {
         const res = await fetch("/api/messages",{
           method: "POST",
@@ -30,6 +41,7 @@ const MessageInput = ({setMessages}) => {
           body: JSON.stringify({
             message: messageText,
             recipientId: selectedConversation.userId,
+            img: imgUrl,
           }),
         })
         const data = await res.json();
@@ -55,32 +67,68 @@ const MessageInput = ({setMessages}) => {
         })
 
         setMessageText("");
+        setImgUrl("");
       } catch (error) {
         showToast("Error", error.message, "error");
+      } finally{
+        setIsSending(false);
       }
     }
   return (
-    <form onSubmit={handleSendMessage}>
+    <Flex gap={2} alignItems={'center'}>
+    <form onSubmit={handleSendMessage} style={{flex: 95}}>
       <InputGroup>
-
-      <Input  w={'full'} placeholder="Type a message"
+      <Input textOverflow={"ellipsis"}  w={'290px'} placeholder="Type a message"
             value = {messageText} onChange = {(e)=> setMessageText(e.target.value) }
-      /> 
+            /> 
       <Flex >
-      <InputRightElement mr={2} gap={2} onClick={handleSendMessage}>
-      <MdEmojiEmotions cursor={'pointer'} onClick={()=> setShowPicker((val) => !val)}/>
-       <IoSendSharp color = 'gray' cursor={'pointer'} />
-    
+      <InputRightElement   onClick={handleSendMessage}>
+      <Flex alignItems={'center'} gap={3} mr={5}>
+      <MdEmojiEmotions  size={20} cursor={'pointer'} onClick={()=> setShowPicker((val) => !val)}/>
+       <IoSendSharp  size={20} color = 'gray' cursor={'pointer'} />
+       </Flex>
       </InputRightElement>
       </Flex>
       </InputGroup>
       <Flex ml={7} mt={2}>
         {showPicker && (
-            <Picker theme="dark"  width={'400px'} height={400} onEmojiClick={onEmojiClick}/>
+          <Picker theme="dark"  width={'400px'} height={400} onEmojiClick={onEmojiClick}/>
         )}
        </Flex>
     </form>
+    <Flex flex={5} cursor={'pointer'} mr={1} mb={1}>
+      <BsFillImageFill size={20} onClick={()=> imageRef.current.click()} />
+        <Input type="file" hidden ref={imageRef} onChange={handleImageChange}/>
+    </Flex>
+
+    <Modal
+				isOpen={imgUrl}
+				onClose={() => {
+					onClose();
+					setImgUrl("");
+				}}
+			>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader></ModalHeader>
+					<ModalCloseButton />
+					<ModalBody>
+						<Flex mt={5} w={"full"}>
+							<Image src={imgUrl} />
+						</Flex>
+						<Flex justifyContent={"flex-end"} my={2}>
+							{!isSending ? (
+								<IoSendSharp size={24} cursor={"pointer"} onClick={handleSendMessage} />
+							) : (
+								<Spinner size={"md"} />
+							)}
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+    </Flex>
   )
 }
 
 export default MessageInput
+
